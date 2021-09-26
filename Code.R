30abde0155ca13eb5628833d17c248fa0b003d97
Hokseson <974766935@qq.com>
2021-09-26 21:53:13 +0100

    Louhokseson

--- Code.R
+++ Code.R
@@ -7,3 +7,30 @@ setwd("D:/EDIN/Sem1/StatisticalProgramming/Coursework1/SPcoursework8thOctober")
 a <- scan("1581-0.txt",what="character",skip=156)
 n <- length(a)
 a <- a[-((n-2909):n)] ## strip license
+
+#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#XXXXXXXXXXXXXXXXXX THE SPLIT-PUNCT XXXXXXXXXXXXXXXXXX
+#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#x <- gsub(ls[i],"",x,fixed=TRUE) ## get rid of punctuation
+
+split_punct <- function(x){
+  #The function should search
+  #for each word containing the punctuation mark, remove it from the word, and add the mark as a new entry
+  #in the vector of words, after the word it came from. 
+  ls <- c(",", ".", ";", "!", ":", "?", "’")
+  lenl <- length(ls)
+  for(i in 1:lenl) {
+    browser()
+    #find the location of the words that contains punctuation(s).
+    a <- grep(ls[i],x,fixed=TRUE)
+    X <- gsub(ls[i],"",x,fixed=TRUE) ## get rid of punctuation
+    lenA <- length(a)
+    Totallen <- lenA + length(x)
+    xs <- rep(0,Totallen) ## vector to store the single digits
+    A <- a + 1:lenA
+    xs[A] <- ls[i]
+    xs[-A] <- X
+    x <- xs
+  }
+  return(x)
+}
